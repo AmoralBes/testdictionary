fcb4d6e041440f0a5cae1111cbd25a317f8907f1
Bes <138616342+AmoralBes@users.noreply.github.com>
2023-07-13 11:17:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Linq;
+using Excel = Microsoft.Office.Interop;
+using Aspose.Cells;
+namespace Slovo
+{
+    
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //string Stroka = "Borisenkov M. F., Popov S. V., Smirnov V. V., Gubin D. G., Petrov I. M., Vasilkova T. N., Solovieva S. V., Martinson E. A., Pechеrkina A. A., Dorogina O. I., Symaniuk E. E.";
+            //Console.WriteLine(Trans.russian.Count);
+            //for (int i = 0; i < Trans.russian.Count;i++) 
+            //{
+            //    int pos = Stroka.IndexOf(Trans.english[i]);
+            //    Stroka = Stroka.Remove(pos, Trans.english[i].Length).Insert(pos, Trans.russian[i]); 
+            //}
+
+            //    Console.WriteLine(Stroka);
+            //for (int i = 0; i < (Trans.russian.Count + Trans.english.Count) / 2; i++)
+            //{
+            //    int pos = Stroka.IndexOf(Trans.russian[i]);
+            //    Stroka = Stroka.Remove(pos, Trans.russian[i].Length).Insert(pos, Trans.english[i]);
+            //}
+            //Console.WriteLine("\n");
+            //Console.WriteLine(Stroka);
+
+            ////foreach (var person in people)
+            ////{
+            ////    Console.WriteLine($"key: {person.Key}  value: {person.Value}");
+            ////}
+            //    foreach (var pers in Trans.english)       
+            //    {
+            //        Console.WriteLine($"key: {pers.Key} value : {pers.Value}");
+            //    }
+
+            Console.WriteLine("Выберите файл\n");
+            string gate; gate = Console.ReadLine();
+            Workbook wb = new Workbook(gate);
+            Worksheet ws = wb.Worksheets[0];
+
+            Console.WriteLine("Выберите колонну\n");                  
+            int I = Convert.ToInt32(Console.ReadLine());
+            I--;
+            //string A = (string)ws.Cells[4, I].Value;
+            //Console.WriteLine(A);
+            Console.WriteLine("Сколько строк содержат заголовкии?\n");
+            int Zagolovki = Convert.ToInt32(Console.ReadLine());
+
+            int Counter_Rows_Sheet = wb.Worksheets[0].Cells.Rows.Count;
+            for (int i = Zagolovki + 1; i < Counter_Rows_Sheet; i++) //Цикл колонн
+            {
+                string A = (string)ws.Cells[i, I].Value;
+                foreach (var family in Trans.Urusification) //Цикл транслитерации (перевода) в колоннах
+                {
+                    int pos = A.IndexOf(Trans.Urusification[family.Key]);
+                    A = A.Remove(pos, Trans.Urusification[family.Key].Length).Insert(pos, Trans.Urusification[family.Value]);
+                }
+                ws.Cells[i, I].Value = A;
+                i++;
+            }
+        }
+    }
+}
---
+++ Trans.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Transactions;
+using Excel = Microsoft.Office.Interop;
+using Aspose.Cells;
+namespace Slovo
+{
+
+    public static class Trans
+    {
+
+        //public static string Translate(int line)
+        //{
+        //    return russian[line];
+        //}
+        //public static Dictionary<int, string> Dict = new Dictionary<int, string>
+        //{
+        //    [2] = "Строка 2",
+        //    [3] = "Строка 3",
+        //    [5] = "Строка 5",
+
+        //};
+        //public static Dictionary<int, string> russian = new Dictionary<int, string>
+        //{   //То что встретит//то что выдаст
+        //    [0] = "Борисенков",
+        //    [1] = "Попов",
+        //    [2] = "Смирнов",
+        //    [3] = "Губин",
+        //    [4] = "Петров",
+        //    [5] = "Василькова",
+        //    [6] = "Соловьёва",
+        //    [7] = "Мартинсон",
+        //    [8] = "Печёркина",
+        //    [9] = "Дорогина",
+        //    [10] = "Симанюк"
+        //};
+        //public static Dictionary<int, string> english = new Dictionary<int, string>
+        //{   //То что встретит//то что выдаст
+        //    [0] = "Borisenkov",
+        //    [1] = "Popov",
+        //    [2] = "Smirnov",
+        //    [3] = "Gubin",
+        //    [4] = "Petrov",
+        //    [5] = "Vasilkova",
+        //    [6] = "Solovieva",
+        //    [7] = "Martinson",
+        //    [8] = "Pechеrkina",
+        //    [9] = "Dorogina",
+        //    [10] = "Symaniuk"
+        //};
+        public static Dictionary<string, string> Urusification = new Dictionary<string, string>
+        {
+            {"Ya","Я"},{"ya","я"},{"Ia","Иа"},{"ia","иа"},
+            {"Oo","Оо"},{"oo","оо"},{"Ee","Ее"},{"ee","ее"},
+            {"Iy","Ий"},{"iy","ий"},{"Yi","йи"},{"yi","йи"},
+            {"Yey","Ей"},{"yey","ей"},{"Ey","Ей"},{"ey","ей"},
+            {"Uy","Уй"},{"uy","уй"},{"Oy","ой"},{"oy","ой"},
+            {"Zh","Ж"},{"zh","ж"},{"Ts","Ц"},{"ts","ц"},
+            {"Sh","Ш"},{"sh","ш"},{"Shch","Щ"},{"shch","щ"},
+            {"Ch","Ч"},{"ch","ч"},
+            {"Ce","Се"},{"ce","се"},{"Cy","Си"},{"cy","си"},{"Ci","Си"},{"ci","си"},
+            {"Kh","Х"},{"kh","х"},
+            {"Yu","Ю"},{"yu","ю"},{"Iu","Ю"},{"iu","ю"},
+            {"Yo","Йо"},{"yo","йо"},
+          // лат /рус|лат / рус
+            {"A","А"},{"a","а"},//
+            {"B","Б"},{"b","б"},//
+            {"C","К"},{"c","к"},//
+            {"D","Д"},{"d","д"},//
+            {"E","Е"},{"e","е"},//
+            {"F","Ф"},{"f","ф"},//
+            {"G","Г"},{"g","г"},//
+            {"H","Х"},{"h","х"},//
+            {"I","И"},{"i","и"},//
+            {"J","Ж"},{"j","ж"},// 
+            {"K","К"},{"k","к"},//
+            {"L","л"},{"l","л"},//
+            {"M","М"},{"m","м"},//
+            {"N","Н"},{"n","н"},//
+            {"O","О"},{"o","о"},//
+            {"P","П"},{"p","п"},//
+            {"Q","К"},{"q","к"},//
+            {"R","р"},{"r","р"},//
+            {"S","С"},{"s","с"},//
+            {"T","Т"},{"t","т"},//
+            {"U","У"},{"u","у"},//
+            {"V","В"},{"v","в"},//
+            {"W","В"},{"w","в"},//
+            {"X","Кс"},{"x","кс"},//
+            {"Y","И"},{"y","и"},//
+            {"Z","з"},{"z","з"},//
+        };
+
+    }
+}
